aa99b6c29b123557fc10cd1f559eedeafccddbd6
Fredrik Stromberg <fredrik314@gmail.com>
2022-06-22 18:13:09 +0100

    updated setup.py with latest supported python versions.

--- setup.py
+++ setup.py
@@ -60,10 +60,9 @@ setup(
     install_requires=get_requirements(),
     classifiers=[
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         #'Development Status :: 1 - Planning',
         #'Development Status :: 2 - Pre-Alpha',
         #'Development Status :: 3 - Alpha',
